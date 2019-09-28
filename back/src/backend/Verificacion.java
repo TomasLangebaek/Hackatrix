bced9b57b41362aaeef51d09d3efd154c6e8c609
gmjimenez <gm.jimenez@uniandes.edu.co>
2019-09-28 12:18:01 -0500

    Integracion

--- back/src/backend/Verificacion.java
+++ back/src/backend/Verificacion.java
@@ -3,14 +3,53 @@ package backend;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.Hashtable;
+import java.util.LinkedList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Verificacion {
 	
+	private LinkedList<String> descriptionImage; 
 	private Hashtable<String, String> tabla;
 
+	public void Carga() {
+		JSONParser parser = new JSONParser();
+		
+		
+		 try (Reader reader = new FileReader("c:\\projects\\test.json")) {
+
+	         JSONArray jsonArray = (JSONArray) parser.parse(reader);
+	         System.out.println(jsonArray);
+	        
+	         int i=0;
+	         while(i<jsonArray.size()) {
+	         JSONObject ob = (JSONObject) jsonArray.get(i);
+	         String description=(String) ob.get("description");
+	         descriptionImage.add(description);
+	         System.out.println(description);
+	         i++;
+	         }
+
+		 }
+		 
+	      catch (IOException e) {
+	         e.printStackTrace();
+	      }
+	      catch (ParseException e) {
+	         e.printStackTrace();
+	     }
+
+	 }
 	public Verificacion() {
+		Carga();
 		tabla = new Hashtable<>();
+		descriptionImage= new LinkedList<String>();
 		cargarTabla();
 	}
 	
